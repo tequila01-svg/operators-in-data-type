7218b4be3554ce13ac74e9c896d3a1d96a0592db
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-08 18:19:47 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+name="student"
+age=10
+is_student=True
+weight=50
+print("name: ",name)
+print("Dta type is: ",type(name))
+print("age: ",age)
+print("Dta type is: ",type(age))
+print("is_student: ",is_student)
+print("Dta type is: ",type(is_student))
+print("weight: ",weight)
+print("Dta type is: ",type(weight))
